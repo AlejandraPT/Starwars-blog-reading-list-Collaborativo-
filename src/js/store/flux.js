507f830e6549eaebc702b54cf89131d9727b4749
AlejandraPT <rockylars29565@gmail.com>
2022-06-28 19:41:38 +0000

    Cambios

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,30 +12,72 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+
+
+
+			characters: [],
+			planets: [],
+			detail:{},
+			favList:[],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			listCharacters: () => {
+				
+					fetch("https://swapi.dev/api/people")
+					.then((res) => res.json())
+					.then(data => setStore({characters: data.results}))
+				
 			},
-			changeColor: (index, color) => {
-				//get the store
+			listPlanets: () => {
+				
+				fetch("https://swapi.dev/api/planets")
+				.then((res) => res.json())
+				.then(data => setStore({planets: data.results}))
+
+				
+			
+		    },
+			details: (type,uid) => {
+				
+				fetch(`https://www.swapi.tech/api/${type}/${uid}`)
+				.then(response => {
+					if (response.ok) {
+						return response.json();
+					} else {
+						setStore({ error: "No se pudo obtener el personaje" });
+					}
+				})
+				.then(data => setStore({ detail: data.result.properties }));
+
+				
+			
+		    },
+			setFavorites: (item) =>{
+
 				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
+				setStore({ favList: [...store.favList, item] });
+
+			},
+			deleteFavorites: (index) =>{
+
+				const store = getStore();
+
+				setStore({ favList: store.favList.filter((favList, i) => i !== index)})
+
+			},
+			changeColor: (index, color) => {
+				const store = getStore();
+
 				const demo = store.demo.map((elm, i) => {
 					if (i === index) elm.background = color;
 					return elm;
 				});
 
-				//reset the global store
 				setStore({ demo: demo });
 			}
 		}
