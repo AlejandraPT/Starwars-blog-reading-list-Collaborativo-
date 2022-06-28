840cd41a16646540bd21455b137961d33aeeb323
ralphyand <MOISELK211@GMAIL.COM>
2022-06-28 20:38:38 +0000

    nav

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,7 +4,7 @@ import React from "react";
 import "../../styles/home.css";
 
 import { CardListCharacters } from "../component/cardListCharacters";
-import { CardListPlanets } from "../component/cardListPlanets";
+
 
 export const Home = () => (
 	<div className="container-fluid ">
